330859d480af41232383a849d01464afa7b5f0e6
Ibne Arif Al Riham <ibnereham@gmail.com>
2025-07-11 20:27:23 +0600

    Addeed the first and alst commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+createBtn.addEventListener("click", () => {
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "images/delete.png"; 
+    inputBox.appendChild(img);
+    notesContainer.prepend(inputBox);
+    updateStorage();
+});
+
+notesContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage();
+    } else if (e.target.tagName === "P") {
+        e.target.onkeyup = function() {
+            updateStorage();
+        }
+    }
+});
